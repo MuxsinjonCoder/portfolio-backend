836bc837347ee92b085fd92fcd02e5250771cd17
MuxsinjonCoder <[muxsincoder@gmail.com](mailto:muxsincoder@gmail.com)>
2025-05-28 15:05:32 +0500

    ok

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ import uploadRoute from "./src/routes/upload.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 
 // Swagger sozlamalari
 const swaggerOptions = {
